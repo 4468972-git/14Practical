cdf01cffc5ee7a135811c766cce4628fbdc7d788
aaronjf-git <aaron.fortun9@gmail.com>
2026-03-02 15:48:53 +0200

    Created openHash class and its constructor

---
+++ src/openHash.java
@@ -0,0 +1,14 @@
+public class openHash {
+    private int m; // list size
+    private String[] keys;
+    private String[] values;
+
+
+    public openHash(int m) {
+        this.m = m;
+        this.keys = new String[m];
+        this.values = new String[m];
+    }
+}
+
+
